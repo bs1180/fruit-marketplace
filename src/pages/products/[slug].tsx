7cf314f5c02c2db7a02a79fae2740ecf50102312
Ben Smith <benedictsmith@gmail.com>
2020-03-08 00:01:17 +0100

    Improve product page

--- src/pages/products/[slug].tsx
+++ src/pages/products/[slug].tsx
@@ -13,15 +13,21 @@ const ProductPage: NextPage<any> = ({ product }) => {
   return (
     <Layout title={product.name}>
       <Link href="/" passHref>
-        <a className="opacity-75 bg-white px-4 py-3 rounded-full shadow">Back</a>
+        <a className="opacity-75 bg-white px-4 py-3 rounded-full shadow absolute m-8">Back</a>
       </Link>
-      <div className="flex max-w-md mx-auto flex-col bg-white shadow-lg items-center rounded-lg p-8">
-        {product.image_url && <img className="rounded-full bg-center bg-cover" alt="" src={product.image_url} />}
-        <div className=" py-4 my-4 px-4">
-          <h1 className="font-bold text-xl mb-2 text-center">{product.name}</h1>
-          <p className="text-gray-700 text-base">{product.description}</p>
+      <div className="flex max-w-md mx-auto items-center justify-center min-h-screen">
+        <div className="w-full flex flex-col bg-white shadow-lg rounded-lg p-8 items-center">
+          {product.image_url && (
+            <div>
+              <img className="rounded-full bg-center bg-cover" alt="" src={product.image_url} />
+            </div>
+          )}
+          <div className=" py-4 my-4 px-4">
+            <h1 className="font-bold text-2xl mb-2 text-center">{product.name}</h1>
+            <p className="text-gray-700 text-base">{product.description}</p>
+          </div>
+          <div>{money(product.price)}</div>
         </div>
-        <div>{money(product.price)}</div>
       </div>
     </Layout>
   );
